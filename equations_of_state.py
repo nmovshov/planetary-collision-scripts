fbe2a3b025dd839f193cc5756e1a13735cb157bf
naor <nmovshov@gmail.com>
2013-07-04 17:29:08 -0700

    '

--- equations_of_state.py
+++ equations_of_state.py
@@ -1,6 +1,7 @@
 #! /auto/proj/nmovshov_hindmost/collisions/SPHERAL/bin/python
 #-------------------------------------------------------------------------------
 # A script demostrating the use of spheral's equation-of-state objects.
+# Run this in iPython for interactive exploration of EOS options in spheral.
 #-------------------------------------------------------------------------------
 from SolidSpheral3d import *
 import Gnuplot
@@ -8,7 +9,7 @@ import Gnuplot
 #-------------------------------------------------------------------------------
 # NAV Show signs of life
 #-------------------------------------------------------------------------------
-print "Playing around with EOSs..."
+print "Loading spheral equations of state..."
 
 #-------------------------------------------------------------------------------
 # NAV Build a Tillotson EOS for common materials
@@ -19,42 +20,15 @@ units = PhysicalConstants(1.0,   # Unit length in meters
                           1.0)   # Unit time in seconds
 etamin, etamax = 0.01, 100.0
 EOSes = [TillotsonEquationOfState(mat, etamin, etamax, units) for mat in mats]
+granite  = EOSes[0]
+basalt   = EOSes[1]
+nylon    = EOSes[2]
+h2oice   = EOSes[3]
+dirtyice = EOSes[4]
+water    = EOSes[5]
+del EOSes
 
 #-------------------------------------------------------------------------------
-# NAV Plot pressure-density-temperature triples for granite
+# NAV Print available materials table
 #-------------------------------------------------------------------------------
-eos = EOSes[0]
-rho0 = eos.referenceDensity
-rhoMin, rhoMax = 0.9*etamin*rho0, 1.1*etamax*rho0
-drho = (rhoMax - rhoMin)/50
-rho = [rhoMin + k*drho for k in range(50)]
-T = range(200,1001,100)
-P = []
-for rhok in rho:
-	for Tj in T:
-		P.append( (rhok, Tj, eos.pressure(rhok,eos.specificThermalEnergy(rhok,Tj))) )
-
-Pplot = Gnuplot.Gnuplot()
-Pplot.xlabel("rho (kg/m^3)")
-Pplot.ylabel("T (K)")
-Pplot(" set grid")
-Pdata = Gnuplot.Data(P)
-Pplot.splot(Pdata, title="Pressure (Pa)")
-
-#-------------------------------------------------------------------------------
-# NAV Plot a P-rho curve for water at reference temperature
-#-------------------------------------------------------------------------------
-eos = EOSes[5]
-T0 = 320
-rho0 = eos.referenceDensity
-rho = [rho0 + j*rho0/8000 for j in range(101)]
-P = []
-for rhoj in rho:
-	P.append( (rhoj,eos.pressure(rhoj,eos.specificThermalEnergy(rhoj,T0))) )
-
-Prplot = Gnuplot.Gnuplot()
-Prplot.xlabel("density (kg/m^3)")
-Prplot.ylabel("Pressure (Pa)")
-Prplot(" set grid")
-Prdata = Gnuplot.Data(P)
-Prplot.plot(Prdata)
+print "options"
---
+++ gnu_plot.py
@@ -0,0 +1,55 @@
+#! /auto/proj/nmovshov_hindmost/collisions/SPHERAL/bin/python
+#-------------------------------------------------------------------------------
+# A script demostrating the use the gnu plot python package.
+#-------------------------------------------------------------------------------
+from SolidSpheral3d import *
+import Gnuplot
+
+#-------------------------------------------------------------------------------
+# NAV Build a Tillotson EOS for common materials
+#-------------------------------------------------------------------------------
+mats = ["Granite", "Basalt", "Nylon", "Pure Ice", "30% Silicate Ice", "Water"]
+units = PhysicalConstants(1.0,   # Unit length in meters
+                          1.0,   # Unit mass in kg
+                          1.0)   # Unit time in seconds
+etamin, etamax = 0.01, 100.0
+EOSes = [TillotsonEquationOfState(mat, etamin, etamax, units) for mat in mats]
+
+#-------------------------------------------------------------------------------
+# NAV Plot pressure-density-temperature triples for granite
+#-------------------------------------------------------------------------------
+eos = EOSes[0]
+rho0 = eos.referenceDensity
+rhoMin, rhoMax = 0.9*etamin*rho0, 1.1*etamax*rho0
+drho = (rhoMax - rhoMin)/50
+rho = [rhoMin + k*drho for k in range(50)]
+T = range(200,1001,100)
+P = []
+for rhok in rho:
+	for Tj in T:
+		P.append( (rhok, Tj, eos.pressure(rhok,eos.specificThermalEnergy(rhok,Tj))) )
+
+Pplot = Gnuplot.Gnuplot()
+Pplot.xlabel("rho (kg/m^3)")
+Pplot.ylabel("T (K)")
+Pplot(" set grid")
+Pdata = Gnuplot.Data(P)
+Pplot.splot(Pdata, title="Pressure (Pa)")
+
+#-------------------------------------------------------------------------------
+# NAV Plot a P-rho curve for water at reference temperature
+#-------------------------------------------------------------------------------
+eos = EOSes[5]
+T0 = 320
+rho0 = eos.referenceDensity
+rho = [rho0 + j*rho0/8000 for j in range(101)]
+P = []
+for rhoj in rho:
+	P.append( (rhoj,eos.pressure(rhoj,eos.specificThermalEnergy(rhoj,T0))) )
+
+Prplot = Gnuplot.Gnuplot()
+Prplot.xlabel("density (kg/m^3)")
+Prplot.ylabel("Pressure (Pa)")
+Prplot(" set grid")
+Prdata = Gnuplot.Data(P)
+Prplot.plot(Prdata)
