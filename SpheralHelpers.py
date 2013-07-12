f487523c4ae2a388d6635798278bbd3193720fe4
naor <nmovshov@gmail.com>
2013-07-12 00:07:07 -0700

    '

---
+++ SpheralHelpers.py
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------------------
+#   Spheral Helpers - A module containing some convenience methods
+#
+#       * pickle_node_list - saves the field variables of a node list
+#------------------------------------------------------------------------------
+import pickle
+
+def pickle_node_list(nl,filename):
+    print 'alo world'
+    print nl.numNodes
+
+
