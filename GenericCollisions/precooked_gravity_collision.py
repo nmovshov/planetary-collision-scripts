c7f0d157e00dc28b93fed2ea2d9853f2973f163f
naor <nmovshov@gmail.com>
2013-08-03 16:02:56 -0700

    p

--- GenericCollisions/precooked_gravity_collision.py
+++ GenericCollisions/precooked_gravity_collision.py
@@ -19,38 +19,52 @@ jobDesc = "load and run precooked planet(s)."
 print '\n', jobName.upper(), '-', jobDesc.upper()
 
 # load precooked lists
-matCore = 'pure ice'
-matMantle = 'basalt'
-targetCoreFile = 'target_core.pnl'
-fid = open(targetCoreFile,'rb')
+matCore = 'basalt'
+matMantle = 'pure ice'
+
+mFile = 'target_core.pnl'
+fid = open(mFile,'rb')
 targetCoreData = shelpers.pickle.load(fid)
 targetCore_pos = targetCoreData['x']
 targetCore_rho = targetCoreData['rho']
-targetCore_mas = targetCoreData['m']
-targetCore_rad = [sqrt(x[0]**2 + x[1]**2 + x[2]**2) for x in targetCore_pos]
+targetCore_m   = targetCoreData['m']
+targetCore_u   = targetCoreData['U']
+targetCore_r   = [sqrt(x[0]**2 + x[1]**2 + x[2]**2) for x in targetCore_pos]
 targetCore_nbNodes = len(targetCore_rho)
+fid.close()
 
-rPlanet = max(targetCore_rad)
-rhoLower = min(targetCore_rho)
+mFile = 'target_mantle.pnl'
+fid = open(mFile,'rb')
+targetMantleData = shelpers.pickle.load(fid)
+targetMantle_pos = targetMantleData['x']
+targetMantle_rho = targetMantleData['rho']
+targetMantle_m   = targetMantleData['m']
+targetMantle_u   = targetMantleData['U']
+targetMantle_r   = [sqrt(x[0]**2 + x[1]**2 + x[2]**2) for x in targetMantle_pos]
+targetMantle_nbNodes = len(targetMantle_rho)
+fid.close()
+
+rPlanet = max(targetMantle_r)
+rhoLower = min(targetMantle_rho)
 rhoUpper = max(targetCore_rho)
-simGlobalNodes = targetCore_nbNodes
+simGlobalNodes = targetCore_nbNodes + targetMantle_nbNodes
 
 # Times, simulation control, and output
 steps = None              # None or advance a number of steps rather than to a time
-goalTime = 1800           # Time to advance to (sec)
+goalTime = 100           # Time to advance to (sec)
 dt = 2                    # Initial guess for time step (sec)
 vizTime = 200             # Time frequency for dropping viz files (sec)
-vizCycle = None           # Cycle frequency for dropping viz files
+vizCycle = 1           # Cycle frequency for dropping viz files
 
 # Node seeding parameters ("resolution")
 nPerh = 1.51              # Nominal number of nodes per smoothing scale
 hmin = 1.0e-6*rPlanet     # Lower bound on smoothing length
 hmax = 1.0e-1*rPlanet     # Upper bound on smoothing length
 rhomin = 0.01*rhoLower    # Lower bound on node density
-rhomax = 4.0*rhoUpper     # Upper bound on node density
+rhomax = 4.*rhoUpper     # Upper bound on node density
 
 # Gravity parameters
-softLength = 1.0e-5       # Fraction of planet radius as softening length
+softLength = 1.0e-6       # Fraction of planet radius as softening length
 opening = 1.0             # Dimensionless opening parameter for gravity tree walk
 fdt = 0.1                 # Gravity timestep multiplier
 softLength *= rPlanet
@@ -60,7 +74,7 @@ G = MKS().G
 dtGrowth = 2.0            # Maximum growth factor for time step in a cycle (dimensionless)
 dtMin = 2                 # Minimum allowed time step (sec)
 dtMax = 1000.0*dt         # Maximum allowed time step (sec)
-verbosedt = False         # Verbose reporting of the time step criteria per cycle
+verbosedt = True         # Verbose reporting of the time step criteria per cycle
 maxSteps = 1000           # Maximum allowed steps for simulation advance
 statsStep = None          # Frequency for sampling conservation statistics and such
 redistributeStep = 2000   # Frequency to load balance problem from scratch
@@ -89,7 +103,7 @@ XSPH = True
 epsilonTensile = 0.3
 nTensile = 4
 HEvolution = IdealH
-densityUpdate = RigorousSumDensity # Sum is best for fluids, integrate for solids
+densityUpdate = IntegrateDensity # Sum is best for fluids, integrate for solids
 compatibleEnergyEvolution = True
 rigorousBoundaries = False
 
@@ -163,8 +177,15 @@ targetCore   = makeFluidNodeList('target_core', eosCore,
                            rhoMax = rhomax,
                            )
 
+targetMantle = makeFluidNodeList('target_mantle', eosMantle, 
+                           nPerh = nPerh, 
+                           hmin = hmin,
+                           hmax = hmax,
+                           rhoMin = rhomin,
+                           rhoMax = rhomax,
+                           )
 
-nodeSet = [targetCore,]
+nodeSet = [targetCore,targetMantle]
 
 # Unless restarting, create the generators and set initial field values.
 if restoreCycle is None:
@@ -173,25 +194,34 @@ if restoreCycle is None:
                                                  rhoUpper,
                                                  distributionType = 'line',
                                                  nNodePerh = nPerh)
-     
-    # Modify geometry. 
-    pass
-
-    # Modify density.
     assert targetCoreGenerator.localNumNodes() == targetCore_nbNodes
+
+    targetMantleGenerator = GenerateNodeDistribution3d(1, 1, targetMantle_nbNodes,
+                                                 rhoUpper,
+                                                 distributionType = 'line',
+                                                 nNodePerh = nPerh)
+    assert targetMantleGenerator.localNumNodes() == targetMantle_nbNodes
+     
+    # Substitute values from pre-cooked data (except density)
     for k in range(targetCore_nbNodes):
         targetCoreGenerator.x[k] = targetCore_pos[k][0]
         targetCoreGenerator.y[k] = targetCore_pos[k][1]
         targetCoreGenerator.z[k] = targetCore_pos[k][2]
-        targetCoreGenerator.m[k] = targetCore_mas[k]
+        targetCoreGenerator.m[k] = targetCore_m[k]
     pass
 
-    # Modify velocity.
+    for k in range(targetMantle_nbNodes):
+        targetMantleGenerator.x[k] = targetMantle_pos[k][0]
+        targetMantleGenerator.y[k] = targetMantle_pos[k][1]
+        targetMantleGenerator.z[k] = targetMantle_pos[k][2]
+        targetMantleGenerator.m[k] = targetMantle_m[k]
     pass
 
    # Fill node lists using generators and distribute to ranks.
     print "Starting node distribution..."
-    distributeNodes3d((targetCore, targetCoreGenerator))
+    distributeNodes3d((targetCore, targetCoreGenerator),
+                      (targetMantle, targetMantleGenerator)
+                      )
      
     nGlobalNodes = 0
     for n in nodeSet:
@@ -205,10 +235,84 @@ if restoreCycle is None:
         nGlobalNodes += mpi.allreduce(n.numInternalNodes, mpi.SUM)
     del n
     print "Total number of (internal) nodes in simulation: ", nGlobalNodes
+
+    # Now fill in pre-cooked density and energy
+    for k in range(targetCore_nbNodes):
+        targetCore.massDensity()[k] = targetCore_rho[k]
+        targetCore.specificThermalEnergy()[k] = targetCore_u[k]
+    pass
+    for k in range(targetMantle_nbNodes):
+        targetMantle.massDensity()[k] = targetMantle_rho[k]
+        targetMantle.specificThermalEnergy()[k] = targetMantle_u[k]
+    pass
     
 # The spheral controller needs a DataBase object to hold the node lists.
 db = DataBase()
 for n in nodeSet:
     db.appendNodeList(n)
 del n
-sys.exit()
+
+# Create the gravity package.
+gravity = OctTreeGravity(G = G, 
+                         softeningLength = softLength, 
+                         opening = opening, 
+                         ftimestep = fdt)
+
+# Create the kernel functions for SPH.
+WT = TableKernel(BSplineKernel(), 1000) # one for normal hydro
+WTPi = WT                               # one for artificial viscosity
+
+# Create the artificial viscosity object.
+q = Qconstructor(Cl, Cq)
+q.limiter = Qlimiter
+q.balsaraShearCorrection = balsaraCorrection
+q.epsilon2 = epsilon2
+q.negligibleSoundSpeed = negligibleSoundSpeed
+q.csMultiplier = csMultiplier
+
+# Create the hydro package.
+hydro = HydroConstructor(WT,
+                         WTPi,
+                         q,
+                         cfl = cfl,
+                         useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                         compatibleEnergyEvolution = compatibleEnergyEvolution,
+                         gradhCorrection = False,
+                         densityUpdate = densityUpdate,
+                         HUpdate = HEvolution,
+                         XSPH = XSPH,
+                         epsTensile = epsilonTensile,
+                         nTensile = nTensile)
+
+# Create the time integrator and attach the physics packages to it.
+integrator = SynchronousRK2Integrator(db)
+integrator.appendPhysicsPackage(gravity)
+integrator.appendPhysicsPackage(hydro)
+integrator.lastDt = dt
+integrator.dtMin = dtMin
+integrator.dtMax = dtMax
+integrator.dtGrowth = dtGrowth
+integrator.verbose = verbosedt
+integrator.rigorousBoundaries = rigorousBoundaries
+
+# Create the controller.
+control = SpheralController(integrator, WT,
+                            statsStep = statsStep,
+                            restartStep = restartStep,
+                            redistributeStep = redistributeStep,
+                            restartBaseName = restartName,
+                            restoreCycle = restoreCycle,
+                            vizBaseName = jobName,
+                            vizDir = vizDir,
+                            vizStep = vizCycle,
+                            vizTime = vizTime)
+
+
+if not steps is None:
+    control.step(steps)
+else:
+    control.advance(goalTime, maxSteps)
+    control.dropRestartFile()
+    control.dropViz()
+pass
+
