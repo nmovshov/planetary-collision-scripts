0c1041af67e82ac0146d49ca1c8fd39292026a5d
naor <nmovshov@gmail.com>
2013-09-09 14:07:25 -0700

    put spheral module under sph namespace and added pext pmin pmax to tillotson eos 	modified:   SpheralPlayground/equations_of_state.py

--- SpheralPlayground/equations_of_state.py
+++ SpheralPlayground/equations_of_state.py
@@ -1,9 +1,13 @@
 #-------------------------------------------------------------------------------
 # Load some of spheral's equation-of-state objects to workspace.
-# Run this in iPython for interactive exploration of EOS options in spheral.
+#
+# Run this script in python (or preferably iPython) to load all the EOS options
+# currently used by uss into the global workspace. There you can interactively
+# call the EOS methods and compare different materials. This script can also be
+# used to extract the code snippets needed to create EOS objects in spheral runs.
 #-------------------------------------------------------------------------------
 import sys, os
-from SolidSpheral3d import *
+import SolidSpheral3d as sph # The top-level spheral module importer
 
 #-------------------------------------------------------------------------------
 # Setup
@@ -12,16 +16,18 @@ from SolidSpheral3d import *
 print "Loading spheral equations of state..."
 
 # EOS constructors take a units object. I usually work in MKS.
-units = PhysicalConstants(1.0,   # Unit length in meters
-                          1.0,   # Unit mass in kg
-                          1.0)   # Unit time in seconds
+units = sph.PhysicalConstants(1.0,   # Unit length in meters
+                              1.0,   # Unit mass in kg
+                              1.0)   # Unit time in seconds
 
 #-------------------------------------------------------------------------------
 # Tillotson EOS for common materials
 #-------------------------------------------------------------------------------
 mats = ['Granite', 'Basalt', 'Nylon', 'Pure Ice', '30% Silicate Ice', 'Water']
 etamin, etamax = 0.01, 100.0
-EOSes = [TillotsonEquationOfState(mat, etamin, etamax, units) for mat in mats]
+pext, pmin, pmax = 2, 3, 4
+EOSes = [sph.TillotsonEquationOfState(mat, etamin, etamax, units, pext, pmin, pmax) 
+         for mat in mats]
 granite  = EOSes[0]
 basalt   = EOSes[1]
 nylon    = EOSes[2]
@@ -33,16 +39,17 @@ del EOSes, mats, etamin, etamax
 #-------------------------------------------------------------------------------
 # M/ANEOS improved SiO2
 #-------------------------------------------------------------------------------
-izetl = vector_of_int(1, -1)
-initializeANEOS('/proj/nmovshov_hindmost/collisions/ANEOS/ANEOS.INPUT', 'ANEOS.barf', izetl)
-SiO2 = ANEOS(0,          # Material number
-             1000,       # num rho vals
-             1000,       # num T vals
-             2000.0,     # minimum density (kg/m^3)
-             4000.0,     # maximum density (kg/m^3)
-             1.0,        # minimum temperature (K)
-             1.0e4,      # maximum temperature (K)
-             units)
+izetl = sph.vector_of_int(1, -1)
+sph.initializeANEOS('/proj/nmovshov_hindmost/collisions/ANEOS/ANEOS.INPUT',
+                                                             'ANEOS.barf', izetl)
+SiO2 = sph.ANEOS(0,          # Material number
+                 1000,       # num rho vals
+                 1000,       # num T vals
+                 2000.0,     # minimum density (kg/m^3)
+                 4000.0,     # maximum density (kg/m^3)
+                 1.0,        # minimum temperature (K)
+                 1.0e4,      # maximum temperature (K)
+                 units)
 os.system('rm -f ANEOS.barf')
 del izetl
 
@@ -52,7 +59,7 @@ del izetl
 K  = 2e5       # polytropic constant
 n  = 1         # polytropic index
 mu = 2.2e-3    # mean molecular weight
-poly = PolytropicEquationOfStateMKS3d(K,n,mu)
+poly = sph.PolytropicEquationOfStateMKS3d(K,n,mu)
 del K, n, mu
 
 #-------------------------------------------------------------------------------
