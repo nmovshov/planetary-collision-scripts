32148862902c6ec3e68f2c8a413d67d0ab57eafa
Naor Movshovitz <nmovshov@gmail.com>
2015-02-10 19:27:19 -0800

    first jab at using numba's JIT acceleration

--- Analysis/bound_mass.py
+++ Analysis/bound_mass.py
@@ -8,7 +8,7 @@ import sys, os, shutil
 import numpy as np
 import argparse
 import ahelpers
-from math import sqrt
+from numba import jit
 cout = sys.stdout.write
 
 def _main():
@@ -342,6 +342,10 @@ def _PCL():
 def _potential(x, y, z, m, mask=None):
     if mask is None:
         mask = np.array(len(x)*[True])
+    return _c_potential(x, y, z, m, mask)
+
+@jit
+def _c_potential(x, y, z, m, mask):
     U = np.zeros(x.shape)
     for j in range(len(x)):
         if mask[j]:
@@ -350,8 +354,7 @@ def _potential(x, y, z, m, mask=None):
                     dx = x[j] - x[k]
                     dy = y[j] - y[k]
                     dz = z[j] - z[k]
-                    dr = (dx*dx + dy*dy + dz*dz)**0.5
-                    #dr = np.sqrt(dx*dx + dy*dy + dz*dz)
+                    dr = np.sqrt(dx*dx + dy*dy + dz*dz)
                     U[j] = U[j] - m[k]/dr
                     U[k] = U[k] - m[j]/dr
                     pass
