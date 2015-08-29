f0c97bb8c6cf3426b83aeb059e4884dc549e35cc
Naor Movshovitz <nmovshov@gmail.com>
2015-08-29 12:20:10 -0700

    fnl_mass.py: cleaned up old ahelpers use and added option for calculating mean radius

--- Analysis/fnl_mass.py
+++ Analysis/fnl_mass.py
@@ -7,42 +7,39 @@
 import sys
 import numpy as np
 import argparse
-#import ahelpers
 
 cout = sys.stdout.write
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help="name of file containing node list data")
+parser.add_argument('-pr', '--print_radii', action='store_true',
+    help="attempt to identify mean radii, assuming spherical geometry")
 args = parser.parse_args()
 
 # Load node list data
 cout("Reading file...")
 try:
-    fnl = ahelpers.load_fnl(args.filename)
+    # FNL structure as it is in 2015-05-01
+    raw = np.loadtxt(args.filename)
     cout("Done.\n")
-    pos = np.vstack((fnl.x, fnl.y, fnl.z)).T
-    vel = np.vstack((fnl.vx, fnl.vy, fnl.vz)).T
-    m   = fnl.m
+    nl_id = raw[:,0]
+    pos = raw[:,2:5]
+    vel = raw[:,5:8]
+    m   = raw[:,8]
     print "Found {} nodes in {} node lists totaling {} kg.".format(
-        fnl.nbNodes, np.unique(fnl.id).size, sum(m))
-    for n in np.unique(fnl.id):
+        nl_id.size, np.unique(nl_id).size, sum(m))
+    for n in np.unique(nl_id):
+        body_nodes = sum(nl_id == n)
+        body_mass = sum(m[nl_id == n])
+        body_pos = pos[nl_id == n]
+        rx = (body_pos[:,0].max() - body_pos[0].min())/2
+        ry = (body_pos[:,1].max() - body_pos[1].min())/2
+        rz = (body_pos[:,2].max() - body_pos[2].min())/2
+        body_r = (rx + ry + rz)/3
         print "    List {:g}: {:.6g} kg in {} nodes ({:.4g} kg/node).".format(
-            n, sum(m[fnl.id == n]), sum(fnl.id == n),
-            sum(m[fnl.id == n])/sum(fnl.id == n))
-except StandardError:
-    try:
-        # FNL structure as it is in 2015-05-01
-        raw = np.loadtxt(args.filename)
-        cout("Done.\n")
-        nl_id = raw[:,0]
-        pos = raw[:,2:5]
-        vel = raw[:,5:8]
-        m   = raw[:,8]
-        print "Found {} nodes in {} node lists totaling {} kg.".format(
-            nl_id.size, np.unique(nl_id).size, sum(m))
-        for n in np.unique(nl_id):
-            print "    List {:g}: {:.6g} kg in {} nodes ({:.4g} kg/node).".format(
-                n, sum(m[nl_id == n]), sum(nl_id == n),
-                sum(m[nl_id == n])/sum(nl_id == n))
-    except:
-        raise StandardError("Could not read data from {}".format(
-            args.filename))
+            n, body_mass, body_nodes, body_mass/body_nodes)
+        if args.print_radii:
+            print "    List {:g}: {:g} m mean radius.".format(n, body_r)
+except:
+    raise StandardError("Could not read data from {}".format(
+        args.filename))
+
