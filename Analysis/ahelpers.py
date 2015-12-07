73f710fd0e725f4312bc681afe3c936345451bc6
Naor Movshovitz <nmovshov@gmail.com>
2015-12-06 20:54:57 -0800

    bringing in Analysis commits from develop

--- Analysis/ahelpers.py
+++ Analysis/ahelpers.py
@@ -132,7 +132,7 @@ def load_multi_fnl(filename):
     else:
         return fnl[0]
 
-def plot_P_vs_r(fnl):
+def plot_P_vs_r(fnl, bblock=False):
     """Plot pressure of nodes against distance from origin."""
 
     import matplotlib as mpl
@@ -144,17 +144,43 @@ def plot_P_vs_r(fnl):
 
     fig = plt.figure()
     axe = plt.axes()
-    plt.xlabel('Radius [m]')
+    plt.xlabel('Radius [km]')
     plt.ylabel('Pressure [GPa]')
     plt.grid()
     for nl in fnl:
         assert isinstance(nl,FNLData)
-        plt.plot(nl.r,nl.P/1e9,'.')
+        x = np.sort(nl.r)
+        y = nl.P[np.argsort(nl.r)]
+        plt.plot(x/1e3, y/1e9)
         pass
-    plt.show(block=False)
+    plt.show(block=bblock)
     return (fig,axe)
 
-def plot_P_vs_r_output(dirname):
+def plot_rho_vs_r(fnl, bblock=False):
+    """Plot density of nodes against distance from origin."""
+
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+
+    assert isinstance(fnl,(FNLData,tuple))
+    if isinstance(fnl,FNLData):
+        fnl = (fnl,)
+
+    fig = plt.figure()
+    axe = plt.axes()
+    plt.xlabel('Radius [km]')
+    plt.ylabel('Mass density [kg/m^3]')
+    plt.grid()
+    for nl in fnl:
+        assert isinstance(nl,FNLData)
+        x = np.sort(nl.r)
+        y = nl.rho[np.argsort(nl.r)]
+        plt.plot(x/1e3, y)
+        pass
+    plt.show(block=bblock)
+    return (fig,axe)
+
+def plot_P_vs_r_output(dirname='.', bblock=False):
     """Plot P(r) for all fnl files in a directory."""
 
     import matplotlib as mpl
@@ -168,6 +194,9 @@ def plot_P_vs_r_output(dirname):
         fnl_files += [os.path.join(root,fn) for fn in files if 
                                                 fn.endswith(('.fnl','.fnl.gz'))]
     fnl_files.sort()
+    if len(fnl_files) == 0:
+        print "No .fnl or .fnl.gz files found in directory."
+        return
     all_fnls = [load_multi_fnl(f) for f in fnl_files]
 
     fig = plt.figure()
@@ -179,11 +208,52 @@ def plot_P_vs_r_output(dirname):
             fnl = (fnl,)
         plt.subplot(nb_rows,nb_cols,k+1)
         for nl in fnl:
-            plt.plot(nl.r,nl.P/1e9,'.')
+            assert isinstance(nl,FNLData)
+            x = np.sort(nl.r)
+            y = nl.P[np.argsort(nl.r)]
+            plt.plot(x/1e3, y/1e9)
             pass
         pass
 
-    plt.show(block=False)    
+    plt.show(block=bblock)    
+    return fig
+
+def plot_rho_vs_r_output(dirname='.', bblock=False):
+    """Plot rho(r) for all fnl files in a directory."""
+
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+
+    assert isinstance(dirname,str)
+    assert os.path.isdir(dirname)
+
+    fnl_files = []
+    for root, dirs, files in os.walk(dirname):
+        fnl_files += [os.path.join(root,fn) for fn in files if 
+                                                fn.endswith(('.fnl','.fnl.gz'))]
+    fnl_files.sort()
+    if len(fnl_files) == 0:
+        print "No .fnl or .fnl.gz files found in directory."
+        return
+    all_fnls = [load_multi_fnl(f) for f in fnl_files]
+
+    fig = plt.figure()
+    nb_rows = np.ceil(np.sqrt(len(all_fnls)))
+    nb_cols = np.ceil(len(all_fnls)/nb_rows)
+    for k in range(len(all_fnls)):
+        fnl = all_fnls[k]
+        if isinstance(fnl,FNLData):
+            fnl = (fnl,)
+        plt.subplot(nb_rows,nb_cols,k+1)
+        for nl in fnl:
+            assert isinstance(nl,FNLData)
+            x = np.sort(nl.r)
+            y = nl.rho[np.argsort(nl.r)]
+            plt.plot(x/1e3, y)
+            pass
+        pass
+
+    plt.show(block=bblock)    
     return fig
 
 def _test():
--- Analysis/plot_P_vs_r.py
+++ Analysis/plot_P_vs_r.py
@@ -1,35 +1,22 @@
-#! /proj/nmovshov_hindmost/collisions/SPHERAL/bin/python
+#!/soft/scipy_0.13.0/CentOS_6/bin/python
 #-------------------------------------------------------------------------------
-# Quick and dirty plot of pressure vs. radius of nodes read from .fnl file.
+# Quick and dirty plot of pressure vs. radius of nodes read from .fnl file(s).
 #-------------------------------------------------------------------------------
 import sys, os
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-pcsbase = '' # Edit this with full path to <pcs> if you see an ImportError.
-sys.path += ['..',pcsbase,os.getenv('PCSBASE','')]
-import shelpers # My module of some helper functions
+import ahelpers
 
 if len(sys.argv)==1:
-    sys.exit("Please provide file name as first parameter")
+    sys.exit("ERROR: provide file/directory name as first parameter.")
 
-nodes = np.loadtxt(sys.argv[1])
-if (nodes.ndim != 2) or (nodes.shape[1] != shelpers.nb_fnl_columns):
-    sys.exit("{} does not appear to contain a valid flattened node list".format(
-             sys.argv[1]))
-
-print "Plotting nodes from file", sys.argv[1]
-x = nodes[:,2]
-y = nodes[:,3]
-z = nodes[:,4]
-r = np.sqrt(x**2 + y**2 + z**2)
-P = nodes[:,10] * 1e-9 # pressure in GPa
-
-plt.figure()
-plt.plot(r,P,'.')
-plt.xlabel('Radius [m]')
-plt.ylabel('Pressure [GPa]')
-plt.title(sys.argv[1])
-plt.grid()
-plt.show()
-print "Done."
+fdname = sys.argv[1]
+if os.path.isfile(fdname):
+    print "Plotting from file", fdname
+    fnl = ahelpers.load_multi_fnl(fdname)
+    ahelpers.plot_P_vs_r(fnl, True)
+elif os.path.isdir(fdname):
+    print "Plotting from directory", fdname
+    ahelpers.plot_P_vs_r_output(fdname, True)
+else:
+    sys.exit("ERROR: {} is not a valid file or directory.".format(fdname))
+    pass
+pass
